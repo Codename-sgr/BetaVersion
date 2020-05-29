d981088e9f63eeecad39e9fcfa1d2ed840ba7bb3
IMBlackPanther <mailsagarat999@gmail.com>
2020-05-29 13:26:43 +0530

    sagar:::DELETE and WishLIst btm added

--- app/src/main/java/com/sagar/betaversion/listAdAdapter.java
+++ app/src/main/java/com/sagar/betaversion/listAdAdapter.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,10 +21,12 @@ public class listAdAdapter extends RecyclerView.Adapter<listAdAdapter.ViewHolder
 
     private List<listAdModel> listAdModelList;
     private RecViewItemClickListener recViewItemClickListener;
+    private Boolean listAd;
 
-    public listAdAdapter(List<listAdModel> listAdModelList,RecViewItemClickListener recViewItemClickListener) {
+    public listAdAdapter(List<listAdModel> listAdModelList,RecViewItemClickListener recViewItemClickListener,Boolean listAd) {
         this.listAdModelList = listAdModelList;
         this.recViewItemClickListener=recViewItemClickListener;
+        this.listAd=listAd;
     }
 
 
@@ -54,6 +58,7 @@ public class listAdAdapter extends RecyclerView.Adapter<listAdAdapter.ViewHolder
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView adProductBrand,adProductModel,adProductPrice,adProductId;
         private ImageView adProductImg;
+        private ImageButton adDelete,adWishlist;
 
 
         public ViewHolder(@NonNull final View itemView) {
@@ -63,6 +68,8 @@ public class listAdAdapter extends RecyclerView.Adapter<listAdAdapter.ViewHolder
             adProductModel=itemView.findViewById(R.id.AdProdModel);
             adProductPrice=itemView.findViewById(R.id.AdProdPrice);
             adProductId=itemView.findViewById(R.id.AdProdId);
+            adDelete=itemView.findViewById(R.id.adDeleteBtn);
+            adWishlist=itemView.findViewById(R.id.adWishListBtn);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -79,6 +86,30 @@ public class listAdAdapter extends RecyclerView.Adapter<listAdAdapter.ViewHolder
             adProductPrice.setText(String.valueOf(adPrice));
             Picasso.get().load(adImg).into(adProductImg);
             adProductId.setText(adId);
+
+            if(listAd){
+                adWishlist.setVisibility(View.VISIBLE);
+                adDelete.setVisibility(View.INVISIBLE);
+            }
+            else{
+                adWishlist.setVisibility(View.INVISIBLE);
+                adDelete.setVisibility(View.VISIBLE);
+            }
+
+            adDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(itemView.getContext(), "Delete", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            adWishlist.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(itemView.getContext(), "Wishlist", Toast.LENGTH_SHORT).show();
+                }
+            });
+
 
 
         }
--- app/src/main/java/com/sagar/betaversion/myAds/myElectronic.java
+++ app/src/main/java/com/sagar/betaversion/myAds/myElectronic.java
@@ -93,7 +93,7 @@ public class myElectronic extends AppCompatActivity implements RecViewItemClickL
                              {
                                  //CODE HERE
                                  Log.i("INFO",model+brand+(price)+img1+ad_id);
-                                 listAdAdapter listAdAdapter=new listAdAdapter(listAdModelList, myElectronic.this);
+                                 listAdAdapter listAdAdapter=new listAdAdapter(listAdModelList, myElectronic.this,false);
                                  recyclerView.setAdapter(listAdAdapter);
                                  listAdAdapter.notifyDataSetChanged();
 
